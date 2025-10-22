a8311bb742ed4ee7a1c6e542adbbf4af28aa5579
Zeusil <abrahemaldaly4@gmail.com>
2025-10-22 21:18:50 +0300

    Delete myproject directory

--- myproject/accounts/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-
-# Create your views here.
-
-def login_view(request):
-    return render(request, 'accounts/login.html')
-
-def register_view(request):
-    return render(request, 'accounts/register.html')
--- myproject/blog/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render, get_object_or_404
-from .models import Article
-
-# Create your views here.
-
-def article_detail(request, pk):
-    article = get_object_or_404(Article, pk=pk)
-    return render(request, 'blog/article_detail.html', {'article': article})
